3c429a55f1d7fcc0d6ffb03ca86dd719bf4e7bef
Michał Bobowski <m_bobowski@o2.pl>
2014-01-14 18:26:24 +0100

    Tabelka z sortowaniem

--- src/view/WordModel.java
+++ src/view/WordModel.java
@@ -21,10 +21,14 @@ public class WordModel extends AbstractTableModel {
         return stats.size();
     }
 
-    @Override
     public int getColumnCount() {
         return columnNames.length;
     }
+    
+    public Class getColumnClass(int c) {  
+    	Object value=this.getValueAt(0,c);  
+    	return (value==null?Object.class:value.getClass());  
+   }
 
     public Object getValueAt(int rowIndex, int columnIndex) {
         WordStats stat = stats.get(rowIndex);
@@ -37,12 +41,10 @@ public class WordModel extends AbstractTableModel {
         }
     }
     
-    @Override
     public String getColumnName(int column) {
         return columnNames[column].toString();
     }
  
-    @Override
     public boolean isCellEditable(int row, int column) {
         return false;
     }
